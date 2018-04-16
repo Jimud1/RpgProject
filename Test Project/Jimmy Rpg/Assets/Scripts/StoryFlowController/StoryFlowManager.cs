3c04eb1d8780b8132a64ff14199371f10fa879eb
Jimud1 <jimud1.james@gmail.com>
2018-04-16 19:05:21 +0100

    updated logic
    sorting out flow of concersations

--- Test Project/Jimmy Rpg/Assets/Scripts/StoryFlowController/StoryFlowManager.cs
+++ Test Project/Jimmy Rpg/Assets/Scripts/StoryFlowController/StoryFlowManager.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 using GoH = Assets.Scripts.Util.GameObjectHelper;
 using Assets.Scripts.Story;
 using Zenject;
+using System;
+
 namespace Assets.Scripts.StoryFlowController
 {
     public class StoryFlowManager : MonoBehaviour, IStoryFlowManager
@@ -14,7 +16,7 @@ namespace Assets.Scripts.StoryFlowController
         public int ConversationChoice = 0;
         public IStoryService _storyService;
         public Button BtnPrefab;
-        public ConversationModel CurrentConversation;
+        private ConversationModel CurrentConversation;
 
         [Inject]
         public void Create(IStoryService storyService)
@@ -26,16 +28,10 @@ namespace Assets.Scripts.StoryFlowController
         {
             _canvas = GoH.AddCanvas(500, 100);
             Canvas = _canvas.GetComponent<Canvas>();
+            GetConversation(ConversationChoice);
             FillCanvas(_canvas);    
         }
         #region Story
-        public void Converse()
-        {
-            if (Canvas.enabled)
-                Canvas.enabled = false;
-            else
-                Canvas.enabled = true;
-        }
         private StoryModel _story;
         public StoryModel CurrentStory
         {
@@ -47,26 +43,34 @@ namespace Assets.Scripts.StoryFlowController
         }
         #endregion
 
+        private ConversationModel GetConversation(int index)
+        {
+            //starts at 0
+            if (index < 0) throw new ArgumentOutOfRangeException("Index is below range");
+            if (CurrentConversation == CurrentStory.Conversations[index]) throw new ArgumentException("Getting same conversation");
+            if (index > CurrentStory.Conversations.Count() - 1) throw new ArgumentOutOfRangeException("Index above range");
+            CurrentConversation = CurrentStory.Conversations[index];
+            return CurrentConversation;
+        }
+
         private void FillCanvas(GameObject canvas)
         {
-
-            CurrentConversation = CurrentStory.Conversations[ConversationChoice];
-            var conversation = CurrentStory.Conversations[ConversationChoice].ConversationText;
+            
+            var conversation = CurrentConversation.ConversationText;
             GoH.AddTextToGameObject(canvas, conversation, 500, 100);
-
-            var convoOptions = CurrentStory.Conversations[ConversationChoice].ConversationOptions;
+            var options = CurrentConversation.ConversationOptions;
             int btnCount = 0;
-            var maxOption = CurrentStory.Conversations.Count();
+            var maxOption = CurrentStory.Conversations.Count() - 1;
             var nextConversation = ConversationChoice + 1;
 
-            foreach (var option in convoOptions)
+            foreach (var option in options)
             {
-                if(nextConversation >= maxOption - 1)
+                if(nextConversation >= maxOption)
                 {
-                    nextConversation = maxOption - 1;
+                    nextConversation = maxOption;
                 }
-                var liveBtn = Instantiate(BtnPrefab);
-                var btn = GoH.UpdateBtn(option, nextConversation, 160, 30, liveBtn, Canvas.gameObject);
+                var btn = Instantiate(BtnPrefab);
+                GoH.UpdateBtn(option, nextConversation, 160, 30, btn, Canvas.gameObject);
                 PositionRect(btn.gameObject, -50 * - (-btnCount));
                 btn.onClick.AddListener(ConversationOnClick);
                 btnCount++;
@@ -82,17 +86,29 @@ namespace Assets.Scripts.StoryFlowController
 
         private void NextStory(int? nextStory)
         {
-            ConversationChoice = -1;
             _story = _storyService.Get((int) nextStory);
+            //Reset after use
+            ConversationChoice = 0;
+            GetConversation(ConversationChoice);
+            isNewStory = true;
         }
 
-
+        private bool isNewStory = false;
         private void ConversationOnClick()
         {  
+            if(isNewStory)
+            {
+                ClearChildren(_canvas.transform);
+                FillCanvas(_canvas);
+                isNewStory = false;
+            }
+
             var go = EventSystem.current.currentSelectedGameObject;
+
             if (go != null)
             {
                 int.TryParse(go.name, out ConversationChoice);
+                GetConversation(ConversationChoice);
                 ClearChildren(_canvas.transform);
                 FillCanvas(_canvas);
             }

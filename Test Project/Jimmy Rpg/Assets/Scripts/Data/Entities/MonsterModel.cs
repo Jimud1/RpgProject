fa84935b70476dcdfb2589f26bfe7334d252db3d
Jimud1 <jimud1.james@gmail.com>
2018-04-20 18:33:52 +0100

    Started work on monsters

--- Test Project/Jimmy Rpg/Assets/Scripts/Data/Entities/MonsterModel.cs
+++ Test Project/Jimmy Rpg/Assets/Scripts/Data/Entities/MonsterModel.cs
@@ -1,11 +1,15 @@
-﻿
+﻿using Newtonsoft.Json;
 using Assets.Scripts.GameLogic;
 
 namespace Assets.Scripts.Data.Entities
 {
     public class MonsterModel : IMonsterModel
     {
+        [JsonProperty("monster_id")]
+        public int MonsterId { get; set; }
+        [JsonProperty("monster_name")]
         public string MonsterName { get; set; }
+        [JsonProperty("monster_description")]
         public string MonsterDescription { get; set; }
         public Statistics Statistics { get; set; }
     }
--- Test Project/Jimmy Rpg/Assets/Scripts/GameLogic/Statistics.cs
+++ Test Project/Jimmy Rpg/Assets/Scripts/GameLogic/Statistics.cs
@@ -3,43 +3,13 @@ namespace Assets.Scripts.GameLogic
 {
     public class Statistics : MonoBehaviour,  IStatistical
     {
-        public float Strength
-        {
-            get; set;
-        }
-
-        public float Dexterity
-        {
-            get; set;
-        }
-        public float Accuracy
-        {
-            get;
-            set;
-        }
-        public float Constitution
-        {
-            get;
-            set;
-        }
-        public float Intelligence
-        {
-            get;
-            set;
-        }
-        public float Wisdom
-        {
-            get;set;
-        }
-        public float MaxHp
-        {
-            get;
-            set;
-        }
-        public float CurrentHp
-        {
-            get;
-            set;
-        }
+        public float Strength { get; set; }
+        public float Dexterity { get; set; }
+        public float Accuracy { get; set; }
+        public float Constitution { get; set; }
+        public float Intelligence { get; set; }
+        public float Wisdom { get; set; }
+        public float MaxHp { get; set; }
+        public float CurrentHp { get; set; }
     }
 }
